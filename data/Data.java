fd17966476cb1b342d1e2c5bf972509cf71a4a7a
suut mulyono <suut.sukses@gmail.com>
2018-10-22 09:44:46 +0700

    awal commit

---
+++ data/Data.java
@@ -0,0 +1,30 @@
+package comp.example.suutmulyono.latihan4.data;
+
+public class Data {
+
+    public  String id, name;
+
+    public Data(){
+    }
+
+    public Data(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
